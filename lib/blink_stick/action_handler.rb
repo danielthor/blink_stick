6040b9a0864e8c318bb446892e98ee5fbff4008f
Daniel Thor <daniel.thor@gmail.com>
2014-02-09 22:47:33 +0100

    very rough fade method
    more of a proof of concept at this time

--- lib/blink_stick/action_handler.rb
+++ lib/blink_stick/action_handler.rb
@@ -13,6 +13,49 @@ module BlinkStick::ActionHandler
     perform_blink(blink_color, current_color, options)
   end
 
+  def fade(color1, color2, options = {})
+    options = { time: 5 }.merge(options)
+
+    # break down colors
+    r1, g1, b1 = color1
+    r2, g2, b2 = color2
+
+    delta_r = r1 - r2
+    delta_g = g1 - g2
+    delta_b = b1 - b2
+    # find greatest distance to travel form one vector value to the other
+    delta_max = [delta_r, delta_g, delta_b].map(&:abs).max
+    puts "delta max = #{delta_max}"
+
+    # in what direction is the vector value moving
+    direction_r = (r1 > r2) ? -1 : 1
+    direction_g = (g1 > g2) ? -1 : 1
+    direction_b = (b1 > b2) ? -1 : 1
+
+    # set initial rgb values
+    r, g, b = color1
+
+    for i in 1..delta_max
+      # find the time to sleep for each loop
+      # so traveling the max distance takes options[:time] to complete
+      sleepy_time = options[:time].to_f/delta_max.to_f
+      puts "sleepy time #{sleepy_time}"
+      sleep sleepy_time
+
+      # set values depending on if it's "time to move"
+      # this will essentially make all the color vectors move at the same pace
+      # greatest distance to travel changes color/moves each loop
+      # while least distance only relative
+      # don't know how else to explain it :(
+      r = r + direction_r if (i % (delta_max/delta_r.abs)) == 0
+      g = g + direction_g if (i % (delta_max/delta_g.abs)) == 0
+      b = b + direction_b if (i % (delta_max/delta_b.abs)) == 0
+
+      puts "color r=#{r} g=#{g} b=#{b}"
+      self.color = [r, g, b]
+    end
+  end
+
   private
 
   def perform_blink(blink_color, current_color, options)
